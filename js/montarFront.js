7e47df9fd4e1e44f3ca738fb4e10d822f506f0ec
biancaEnicole <trabalholourdaobinibini@gmail.com>
2025-05-19 11:52:54 -0300

    08-Criando o arquivo montarFront.js -Usando o objeto Shows- linkando com a área de próximos shows do menu

--- js/montarFront.js
+++ js/montarFront.js
@@ -1,2 +1,28 @@
 import { shows } from "./objetos.js";
-console.log(shows);
+
+// Montando o grid de shows
+// passando o gridShows para o JS, sera nele que os cards irão ficar
+const gridShows = document.querySelector('#gridShows');
+// montando os shows dentro dos cards
+shows.forEach(show => {
+    gridShows.innerHTML += `
+    <div class="col-md-6 col-lg-4 my-3">
+            <div class="card">
+                <a href=""class="text-decoration-none text-dark">
+                <img src="${show.banner}" class="card-img-top" alt="${show.artista}">
+                <div class="card-body">
+                    <h4 class="my-3 bd-bottom">${show.artista}</h4>
+                   <div class="d-flex justify-content-between">
+                    <p>Local: ${show.local}</p>
+                    <p>Data: ${show.data}</p>
+
+                   </div>
+                    <div class="d-flex justify-content-between">
+                        <p>Horário: ${show.horario}</p>
+                        <p>Ingresso: ${show.tipo_ingresso} </p>
+                    </div>
+                </div>
+            </a>
+            </div>
+        </div>`
+})
